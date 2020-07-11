c00a13647f71baf3a53d75258d2c8b05ff27d3d9
Smit Desai <smit.desai16@gmail.com>
2020-07-11 14:40:01 -0500

    Adding facebook shimmer

--- app/src/main/java/com/bombaytone/androidskeleton/MainActivity.java
+++ app/src/main/java/com/bombaytone/androidskeleton/MainActivity.java
@@ -2,7 +2,12 @@ package com.bombaytone.androidskeleton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView3 = findViewById(R.id.tvTitle);
+        textView3.setBackgroundColor(Color.parseColor("#dddddd"));
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        tvDescription.setBackgroundColor(Color.parseColor("#dddddd"));
+        ImageView ivDisplayPic = findViewById(R.id.ivDisplayPic);
+        ivDisplayPic.setBackgroundColor(Color.parseColor("#dddddd"));
+
+        ShimmerFrameLayout sflRoot = findViewById(R.id.sflRoot);
+        // container.startShimmer();
+        sflRoot.startShimmer();
     }
 }
