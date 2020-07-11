4a71d3fd78f9ab839544ffccb687c56cfed82ed2
Smit Desai <smit.desai16@gmail.com>
2020-07-11 14:46:47 -0500

    Implementing complete skeleton loading

--- app/src/main/java/com/bombaytone/androidskeleton/MainActivity.java
+++ app/src/main/java/com/bombaytone/androidskeleton/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bombaytone.androidskeleton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,19 +14,55 @@ import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView3 = findViewById(R.id.tvTitle);
-        textView3.setBackgroundColor(Color.parseColor("#dddddd"));
-        TextView tvDescription = findViewById(R.id.tvDescription);
+        final TextView tvTitle = findViewById(R.id.tvTitle);
+        tvTitle.setBackgroundColor(Color.parseColor("#dddddd"));
+        final TextView tvDescription = findViewById(R.id.tvDescription);
         tvDescription.setBackgroundColor(Color.parseColor("#dddddd"));
-        ImageView ivDisplayPic = findViewById(R.id.ivDisplayPic);
+        final ImageView ivDisplayPic = findViewById(R.id.ivDisplayPic);
         ivDisplayPic.setBackgroundColor(Color.parseColor("#dddddd"));
 
-        ShimmerFrameLayout sflRoot = findViewById(R.id.sflRoot);
-        // container.startShimmer();
+        final ShimmerFrameLayout sflRoot = findViewById(R.id.sflRoot);
         sflRoot.startShimmer();
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+
+                tvTitle.setBackgroundColor(Color.TRANSPARENT);
+                tvTitle.setText("This is title");
+                tvDescription.setBackgroundColor(Color.TRANSPARENT);
+                tvDescription.setText("This is description");
+                ivDisplayPic.setBackground(getDrawable(R.drawable.ic_launcher_background));
+
+                sflRoot.hideShimmer();
+            }
+        };
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long futureTime = System.currentTimeMillis() + 6000;
+                while (System.currentTimeMillis() < futureTime) {
+                    synchronized (this) {
+                        try {
+                            wait(futureTime - System.currentTimeMillis());
+                        }catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                handler.sendEmptyMessage(0);
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
